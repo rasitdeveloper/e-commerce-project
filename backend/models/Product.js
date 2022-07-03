16f7153a552884cf5612ab4af3b6db235fd8d3d6
rasitdeveloper <frasitkilic@gmail.com>
2022-07-03 23:59:22 +0300

    Add product model

---
+++ backend/models/Product.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  photos: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Product = mongoose.model('product', ProductSchema);
+
+module.exports = Product;
