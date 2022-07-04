c38c1bf48350d15e8a43d8b727edb8f9379f03ef
rasitdeveloper <frasitkilic@gmail.com>
2022-07-04 05:20:04 +0300

    Add navigation bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavigationBar from "./components/NavigationBar";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <NavigationBar />
+        <div id="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>
 }
 
 export default App;
---
+++ client/src/components/NavigationBar/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import styles from "./styles.module.css";
+import { Link } from "react-router-dom"
+import { Button } from "@chakra-ui/react"
+
+function NavigationBar() {
+  return (
+    <nav className={styles.nav}>
+        <div className={styles.left}>
+          <div className='logo'>
+            <Link to="/">ReadyTrade</Link>
+          </div>
+          <ul className={styles.menu}>
+            <li>
+              <Link to="/">Products</Link>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.right}>
+          <Link to="/register">
+            <Button colorScheme="cyan">Register</Button>
+          </Link>
+          <Link to="/login">
+            <Button colorScheme="green">Login</Button>
+          </Link>
+          
+        </div>
+    </nav>
+  )
+}
+
+export default NavigationBar
